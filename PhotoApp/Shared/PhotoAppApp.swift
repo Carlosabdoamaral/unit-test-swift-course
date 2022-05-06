80c266ac26177e7bcb6470158928fac8c92f00fc
Carlos_A_Amaral <carlos.a.amaral@sesisc.org.br>
2022-05-06 00:37:55 -0300

    Adding files

---
+++ PhotoApp/Shared/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  Shared
+//
+//  Created by Carlos Amaral on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
